bbb1557224c406e1b9a4b5b1a6e70ea0ac04d1d3
DongGu Kang <ehdrnsla3020@naver.com>
2017-06-13 09:58:22 +0900

    Create SaveDateToCSVFile.sql

--- SaveDateToCSVFile.sql
+++ SaveDateToCSVFile.sql
@@ -1,5 +1,7 @@
+#If you don't know where is your DB File path, select @@datadir 
+
 SELECT * 
 FROM CONNECTOR 
-INTO OUTFILE 'C:\connector.csv' 
+INTO OUTFILE 'connector.csv' 
 FIELDS TERMINATED BY ',' 
 LINES TERMINATED BY ';'; 
