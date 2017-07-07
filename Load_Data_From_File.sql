1a86259223923ac0fc3e2d3efcaef7d329b16a9c
DongGu Kang <ehdrnsla3020@naver.com>
2017-07-07 19:08:40 +0900

    Update Load_Data_From_File.sql

--- Load_Data_From_File.sql
+++ Load_Data_From_File.sql
@@ -4,3 +4,4 @@
 LOAD DATA (LOCAL) INFILE 'D:\data/fns_u_invest_20170530.DAT' INTO TABLE FMKSP200_DATA
 FIELDS TERMINATED BY '|'
 LINES TERMINATED BY '\n';
+#(@col1,@col2,@col3,@col4) set name=@col4,id=@col2 ; 
